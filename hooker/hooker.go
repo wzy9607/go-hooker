d8a3ae56c8bac941952c6d377eb6398faa1f1575
Cattī Crūdēlēs <17695588+wzy9607@users.noreply.github.com>
2025-07-28 10:58:37 +0800

    feat: first add, first execute

--- hooker/hooker.go
+++ hooker/hooker.go
@@ -1,40 +1,61 @@
 package hooker
 
-type Hook[Func any] func(Func) Func
+import (
+	"slices"
+	"sync"
+)
+
+type Hook[Func any] func(next Func) Func
 
 type Hooker[Func any] struct {
+	mu sync.RWMutex
+
 	origin  Func         // 原始函数
 	wrapped Func         // 包装后的函数
 	hooks   []Hook[Func] // 钩子函数列表
 }
 
-func NewHooker[Func any](origin Func, hooks ...Hook[Func]) *Hooker[Func] {
+func NewHooker[Func any](origin Func) *Hooker[Func] {
 	h := &Hooker[Func]{
-		origin: origin,
-		hooks:  hooks,
+		mu:      sync.RWMutex{},
+		origin:  origin,
+		wrapped: origin,
+		hooks:   make([]Hook[Func], 0),
 	}
-	wrapped := origin
-	for _, hook := range h.hooks {
-		wrapped = hook(wrapped)
-	}
-	h.wrapped = wrapped
 	return h
 }
 
-func (h *Hooker[Func]) AddHook(hook Hook[Func]) *Hooker[Func] {
-	h.hooks = append(h.hooks, hook)
-	h.wrapped = hook(h.wrapped)
+func (h *Hooker[Func]) chain() {
+	h.wrapped = h.origin
+	for i := len(h.hooks) - 1; i >= 0; i-- {
+		h.wrapped = h.hooks[i](h.wrapped)
+	}
+}
+
+func (h *Hooker[Func]) AddHook(hook ...Hook[Func]) *Hooker[Func] {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	h.hooks = append(h.hooks, hook...)
+	h.chain()
 	return h
 }
 
 func (h *Hooker[Func]) GetOrigin() Func {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+
 	return h.origin
 }
 
 func (h *Hooker[Func]) GetWrapped() Func {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
 	return h.wrapped
 }
 
 func (h *Hooker[Func]) GetHooks() []Hook[Func] {
-	return h.hooks
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+	return slices.Clone(h.hooks)
 }
--- hooker/hooker_test.go
+++ hooker/hooker_test.go
@@ -3,43 +3,102 @@ package hooker
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-// Func 原始函数原型
-type Func func(a, b int) int
-
-// SomeFunc 原始函数定义
-func SomeFunc(a, b int) int {
-	fmt.Printf("%d + %d\n", a, b)
-	return a + b
-}
-
-func Hook1(next Func) Func {
-	return func(a, b int) int {
-		fmt.Println("----- Hook1 before ----")
-		c := next(a, b)
-		fmt.Println("----- Hook1 after ----")
-		return c
+func TestHooker_AddHook(t *testing.T) {
+	t.Parallel()
+	type handler func(a int) int
+	hooker := NewHooker[handler](func(a int) int {
+		assert.Equal(t, 6, a)
+		return a + 4
+	})
+	hook1 := func(next handler) handler {
+		return func(a int) int {
+			assert.Equal(t, 0, a)
+			return next(a + 1)
+		}
 	}
-}
-
-func Hook2(next Func) Func {
-	return func(a, b int) int {
-		fmt.Println("----- Hook2 before ----")
-		c := next(a, b)
-		fmt.Println("----- Hook2 after ----")
-		return c
+	hook2 := func(next handler) handler {
+		return func(a int) int {
+			assert.Equal(t, 1, a)
+			return next(a + 2)
+		}
 	}
+	hook3 := func(next handler) handler {
+		return func(a int) int {
+			assert.Equal(t, 3, a)
+			return next(a + 3)
+		}
+	}
+	hooker.AddHook(hook1, hook2)
+	hooker.AddHook(hook3)
+
+	b := hooker.GetWrapped()(0)
+	assert.Equal(t, 10, b)
+
+	c := hooker.GetOrigin()(6)
+	assert.Equal(t, 10, c)
 }
 
-func TestNewHooker(t *testing.T) {
-	hooker := NewHooker[Func](SomeFunc, Hook1, Hook2)
-	hooker.GetWrapped()(1, 2)
+func ExampleHooker_AddHook() {
+	type handler func(a, b int) int
+	hooker := NewHooker[handler](func(a, b int) int {
+		fmt.Printf("%d + %d\n", a, b)
+		return a + b
+	})
+	hook1 := func(next handler) handler {
+		return func(a, b int) int {
+			fmt.Println("----- hook1 before ----")
+			c := next(a, b)
+			fmt.Println("----- hook1 after ----")
+			return c
+		}
+	}
+	hook2 := func(next handler) handler {
+		return func(a, b int) int {
+			fmt.Println("----- hook2 before ----")
+			c := next(a, b)
+			fmt.Println("----- hook2 after ----")
+			return c
+		}
+	}
+	hook3 := func(next handler) handler {
+		return func(a, b int) int {
+			fmt.Println("----- hook3 before ----")
+			c := next(a, b)
+			fmt.Println("----- hook3 after ----")
+			return c
+		}
+	}
+	hooker.AddHook(hook1, hook2)
+	hooker.AddHook(hook3)
+	c := hooker.GetWrapped()(1, 2)
+	fmt.Println(c)
+	// Output:
+	// ----- hook1 before ----
+	// ----- hook2 before ----
+	// ----- hook3 before ----
+	// 1 + 2
+	// ----- hook3 after ----
+	// ----- hook2 after ----
+	// ----- hook1 after ----
+	// 3
 }
 
-func TestAddHook(t *testing.T) {
-	hooker := NewHooker[Func](SomeFunc)
-	hooker.AddHook(Hook1)
-	hooker.AddHook(Hook2)
-	hooker.GetWrapped()(1, 2)
+func TestHooker_GetHooks(t *testing.T) {
+	t.Parallel()
+	type handler func(a int) int
+	hooker := NewHooker[handler](func(a int) int { return a })
+	hooker.AddHook(func(next handler) handler { return func(a int) int { return next(a) } })
+	hooker.AddHook(func(next handler) handler { return func(a int) int { return next(a) } })
+	hooks := hooker.GetHooks()
+	assert.Equal(t, 2, len(hooks))
+
+	// test hooks are cloned
+	hooks[1] = func(next handler) handler { return func(a int) int { return next(a + 1) } }
+	hooker.AddHook(func(next handler) handler { return func(a int) int { return next(a) } })
+	c := hooker.GetWrapped()(0)
+	assert.Equal(t, 0, c)
 }
